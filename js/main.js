39fca5d2f203671f90b524f196e06f7a9b9ed9fa
sarguments <sarguments@gmail.com>
2026-09-14 16:52:03 +0900

    Fix: 미사용 상수 제거

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// 재할당하지 않는 값은 const가 기본이다. var는 쓰지 않는다.
-const statusMessage = 'B1-1 skeleton loaded';
-
 // HTML에 onclick을 쓰지 않고 여기서 이벤트를 연결한다.
 // DOMContentLoaded는 "HTML 해석이 끝난 뒤"를 보장하는 신호다.
 // (defer로 연결했으므로 순서도 보장된다)
